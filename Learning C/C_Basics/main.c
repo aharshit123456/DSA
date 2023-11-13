3acf225526c52eb3fb17f0330654c875ee1fb003
Harshit Agarwal <aharshit123456@gmail.com>
2023-11-14 02:57:23 +0530

    Added Learning C files

---
+++ Learning C/C_Basics/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double cube(double num){
+    double result = num*num*num ;
+    return result;
+}
+
+
+int main()
+{
+//    char characterName[] = "Tom";
+//    int characterAge = 67;
+//    printf("There once was a man named %s .\n", characterName);
+//    printf("He was %d years old.\n", characterAge);
+//    characterAge = 34;\\
+//    printf("He really liked the name %s.\n", characterName);
+//    printf(" %d\n", characterAge);
+//
+//
+//    int age = 40;
+//    double gpa = 3.0;
+//    char grade = 'A';
+//    char phrase[] = "Giraffe Academy";
+
+//    printf("Hello World \n");
+//    printf("%d %s",45,"number");
+
+
+// inputs scanf,fgets
+
+//fgets char stdin also includes "Enter" as newline
+
+// Building a basic calculator
+//    double num1;
+//    double num2;
+//    printf("Enter first number: ");
+//    scanf("%lf", &num1);
+//    printf("Enter second number: ");
+//    scanf("%lf", &num2);
+//    printf("Answer: %10.3f", num1 + num2) ;
+//    return 0;
+
+// arrays -> int array[] = {3,4,5} ;
+
+//    char name[20];
+//    printf("What is your name ?\n My name is: ");
+//    fgets(name,20,stdin);
+//    sayHi(name);
+
+    printf("Answer: %f", cube(3.0));
+    return 0;
+}
+
+//void sayHi(char name[]){
+//    printf("Hello %s",name);
+//}
